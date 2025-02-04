580ac574d773ff79a808589a753f4adddc09b6b5
Deluxt <dustinwidnerdev@gmail.com>
2025-02-04 02:13:48 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,7 @@ function Header() {
       <div className="row">
         <div className="col-12 p-3">
           <h1>
-            <i className="fa-solid fa-gears mx-1 fa-xs"></i>Deluxt.dev - Powered
-            By Netlify.
+            <i className="fa-solid fa-gears mx-1 fa-xs"></i>Deluxt's Mind
           </h1>
         </div>
       </div>
